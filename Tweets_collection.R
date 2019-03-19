e6c53a4d2ceb5831fb8ff2c22c002874a16cceaf
pallavis-webonise <pallavi.srikhakollu@weboniselab.com>
2019-03-19 01:14:10 -0400

    Added Ny times data collection and modified the twitter api

--- Tweets_collection.R
+++ Tweets_collection.R
@@ -14,25 +14,25 @@ access_secret_token = Sys.getenv("TWITTER_ACCESS_SECRET")
 setup_twitter_oauth(consumer_key,consumer_secret_key,access_token,access_secret_token)
 
 #######################################################################################
-tweets <- searchTwitter("#ChampionsLeague",  lang = "en", n = 15000)
+tweets <- searchTwitter("#NFL",  lang = "en", n = 15000)
 tweets.df <-twListToDF(tweets)
 #write.table(tweets.df, "Documents/DIC/tweets.csv", sep = ",", col.names = T, append = T)
-if (file.exists("/Users/Pallavi/Documents/DIC_project_2/ChampionsLegueTweets.csv")) {
-  write.table(tweets.df, file = "/Users/Pallavi/Documents/DIC_project_2/ChampionsLegueTweets.csv", append = TRUE, col.names = FALSE, row.names = FALSE, sep = ",")
+if (file.exists("/Users/Pallavi/Documents/DIC-Project-2/NFLTweets.csv")) {
+  write.table(tweets.df, file = "/Users/Pallavi/Documents/DIC-Project-2/NFLTweets.csv", append = TRUE, col.names = FALSE, row.names = FALSE, sep = ",")
 } else {
-  write.table(tweets.df, file = "/Users/Pallavi/Documents/DIC_project_2/ChampionsLegueTweets.csv", col.names = TRUE, row.names = FALSE, sep = ",")
+  write.table(tweets.df, file = "/Users/Pallavi/Documents/DIC-Project-2/NFLTweets.csv", col.names = TRUE, row.names = FALSE, sep = ",")
 }
 
 
 
 ############################
-tweets_r.df <- read.csv("/Users/Pallavi/Documents/DIC_project_2/ChampionsLegueTweets.csv",row.names = NULL)
+tweets_r.df <- read.csv("/Users/Pallavi/Documents/DIC-Project-2/NFLTweets.csv",row.names = NULL)
 tweets_1 <- tweets_r.df %>% distinct(id,.keep_all = TRUE)
 
 tweets_location_unique <- tweets_1 %>%  filter((isRetweet == FALSE),1,all())
 
 if (file.exists("/Users/Pallavi/Documents/DIC_project_2/FilteredTweets.csv")) {
-  write.table(tweets_location_unique, file = "/Users/Pallavi/Documents/DIC_project_2/FilteredTweets.csv", append = TRUE, col.names = FALSE, row.names = FALSE, sep = ",")
+  write.table(tweets_location_unique, file = "/Users/Pallavi/Documents/DIC-Project-2/NFLFilteredTweets.csv", append = TRUE, col.names = FALSE, row.names = FALSE, sep = ",")
 } else {
-  write.table(tweets_location_unique, file = "/Users/Pallavi/Documents/DIC_project_2/FilteredTweets.csv", col.names = TRUE, row.names = FALSE, sep = ",")
+  write.table(tweets_location_unique, file = "/Users/Pallavi/Documents/DIC-Project-2/NFLFilteredTweets.csv", col.names = TRUE, row.names = FALSE, sep = ",")
 }
